ec2b4d3eef941e101536cd3213908139b4a6ff96
andrevier <rangel_andre@outlook.com>
2023-03-30 06:07:37 -0300

    Issue #2: Implement delete functionality.
    The delete function deletes a row in the html of the table, delete the project in the array of projects, and update the row number.

--- frontend/packages/lib.js
+++ frontend/packages/lib.js
@@ -1,7 +1,45 @@
 // projects.html
 
-// Set the new project form to invisible beforehand.
+// Set the new project form to invisible.
 document.getElementById("newProjectFormId").style.display = "none";
+
+// Array to save the json with data from the projects.
+var projectsArray = [
+    {
+        number: 1,
+        name: "My Project",
+        id: "MP",
+        deadline: "2023-03-29",
+        description: "About freedom."
+    },
+    {
+        number: 2,
+        name: "Buy a car",
+        id: "MP",
+        deadline: "2023-04-08",
+        description:"Save money."
+    },
+    {
+        number: 3,
+        name: "Merry in Canada",
+        id: "MC",
+        deadline: "2023-04-08",
+        description:"Save money."
+    },
+    {
+        number: 3,
+        name: "Visit Gana",
+        id: "VG",
+        deadline: "2023-12-08",
+        description:"Save money."
+    }
+];
+
+updateHtmlTableRows();
+
+
+// Library
+
 
 function displayNewProjectForm() {
     console.log("displayNewProjectForm clicked");
@@ -22,6 +60,7 @@ function eraseProjectFormFields() {
     document.getElementById("newProjectDescription").value = "";
 
 }
+
 function createNewProject() {
     // Project's data.
     const newProjectName = document.getElementById("newProjectName");
@@ -32,22 +71,85 @@ function createNewProject() {
     // Get table's html content.
     const projectsTableBody = document.getElementById("projectsTableBody");
     
+    let projectJson = {
+        number: projectsArray.length,
+        name: newProjectName.value,
+        id: newProjectID.value,
+        deadline: newProjectDeadline.value,
+        description: newProjectDescription.value
+    };
+    
+    // Push the project's data to the array of projects.
+    projectsArray.push(projectJson);
+
     // Create a table row with the new data.
     let row = `
     <tr>
-        <th scope="row">1</th>
+        <th scope="row">${projectsArray.length}</th>
         <td>${newProjectName.value}</td>
         <td>${newProjectID.value}</td>
         <td>${newProjectDeadline.value}</td>
         <td>
         <div >
-        <button type="button" class="btn btn-link"><img src="packages/node_modules/bootstrap-icons/icons/pencil.svg" alt="Edit" width="20"></button>
-        <button type="button" class="btn btn-link"><img src="packages/node_modules/bootstrap-icons/icons/trash3.svg" alt="Edit" width="20"></button>
+        <button type="button" class="btn btn-link" onclick="editRowButtonClicked()"><img src="packages/node_modules/bootstrap-icons/icons/pencil.svg" alt="Edit" width="20"></button>
+        <button type="button" class="btn btn-link" onclick="deleteRow(${projectsArray.length})"><img src="packages/node_modules/bootstrap-icons/icons/trash3.svg" alt="Edit" width="20"></button>
         </div>
         </td>
     </tr>`;
-    console.log(row);
+
+    // Add the html to the table.
     projectsTableBody.innerHTML += row;
+
     eraseProjectFormFields();
     returnToProjectTable();
+
+    console.log(projectsArray);
+}
+
+function editRowButtonClicked() {
+    console.log("Clicked!")
+}
+
+function updateRowNumber() {
+    for (let i = 0; i < projectsArray.length; i++) {
+        projectsArray[i].number = i + 1;
+    };
+
+}
+
+function updateHtmlTableRows() {
+    const projectsTableBody = document.getElementById("projectsTableBody");
+    projectsTableBody.innerHTML = "";
+    for (let i = 0; i < projectsArray.length; i++) {
+        const row = `
+        <tr>
+            <th scope="row">${projectsArray[i].number}</th>
+            <td>${projectsArray[i].name}</td>
+            <td>${projectsArray[i].id}</td>
+            <td>${projectsArray[i].deadline}</td>
+            <td>
+            <div >
+            <button type="button" class="btn btn-link" onclick="editRowButtonClicked()"><img src="packages/node_modules/bootstrap-icons/icons/pencil.svg" alt="Edit" width="20"></button>
+            <button type="button" class="btn btn-link" onclick="deleteRow(${projectsArray[i].number})"><img src="packages/node_modules/bootstrap-icons/icons/trash3.svg" alt="Edit" width="20"></button>
+            </div>
+            </td>
+        </tr>`;
+        projectsTableBody.innerHTML += row;
+    };     
+}
+
+function deleteRow(rowNumber) {
+    console.log("delete row number ", rowNumber);
+    console.log("Array before ", projectsArray);
+
+    // Remove the item from the array.
+    projectsArray.splice(rowNumber - 1, 1);
+    console.log("Array after ", projectsArray);
+
+    document.getElementsByTagName("tr")[rowNumber].remove();
+    
+    updateRowNumber();
+
+    updateHtmlTableRows();
+
 }
