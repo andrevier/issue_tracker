7f60b4e4acde818bcbb8675afd7f3ea240aa828c
andrevier <rangel_andre@outlook.com>
2023-04-14 15:39:18 -0300

    Issue #11: add default message to warn the user that a field is empty at the login page.

--- frontend/src/scripts/login.js
+++ frontend/src/scripts/login.js
@@ -1,41 +1,26 @@
 // Login logic.
 // Dev: Get the users' data from the json file.
-const response = await fetch('./scripts/fakeUsers.json');
-const fakeUsers = await response.json();
+const loginButton = document.getElementById("login-button");
+const email = document.getElementById("user-email");
+const password = document.getElementById("user-password");
+const emailHelper = document.getElementById("email-helper");
+const passwordHelper = document.getElementById("password-helper");
 
-const loginButton = document.getElementById("loginButton");
-const userEmail = document.getElementById("userEmail");
-const userPassword = document.getElementById("userPassword");
-
-loginButton.addEventListener("click", function (e) {
+loginButton.addEventListener("click", function(e) {
     e.preventDefault();
-    
-    // Basic authentication.    
-    console.log('login button clicked');
-    const userId = getUserId(userEmail.value, userPassword.value);
-    console.log('userId: ' + userId);
-    if (userId > 0) {
-        // Pass the user Id to the local storage.
-        window.localStorage.setItem('userId', userId);
-
-        // Go to the project's page.
-        window.location.href = './projects.html';
-
-    } else {
-        console.log('wrong user data');
+    if (email.value === "" && password.value === "") {
+        emailHelper.innerHTML = "email is required";
+        passwordHelper.innerHTML = "password is required";
+        email.focus();
+        return false;
+    } else if (email.value === "") {
+        emailHelper.innerHTML = "email is required";
+        email.focus();
+        return false;
+    } else if (password.value === "") {
+        passwordHelper.innerHTML = "password is required";
+        password.focus();
+        return false;
     }
-   
-});
-
-function getUserId(email, password) {
-    // Authentication logic.
-    console.log('checking user data');
-
-    for (let i = 0; i < fakeUsers.length; i++) {
-        if (email === fakeUsers[i].email && password === fakeUsers[i].password) {
-            return fakeUsers[i].id;
-        }
-    }
-    return -1;
-}
+})
 
